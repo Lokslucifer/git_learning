7fbf382c2cc7de9b68919200cca68376bbf4f705
lokesh <lok8695esh@gmail.com>
2025-06-25 11:00:12 +0530

    updated gitworkflow

--- internals/constants/app_constant.go
+++ internals/constants/app_constant.go
@@ -3,7 +3,7 @@ package constants
 const (
 	AppName     = "MyApp"
 	Version     = "v1.0.0"
-	DefaultPort = ":8081"
+	DefaultPort = "0.0.0.0:8081"
 
 	//Key for JWT Claim
 	ClaimPrimaryKey = "id"
@@ -11,7 +11,5 @@ const (
 	SuccessMessage = "success"
 
 	//Dburl
-	DBURL="user=postgres password=yourpassword dbname=wetransfer host=localhost port=5432 sslmode=disable"
-	
-	
+	DBURL = "user=postgres password=yourpassword dbname=wetransfer host=db port=5432 sslmode=disable"
 )
